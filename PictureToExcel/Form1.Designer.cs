79039b664e51d6af9553ea0c0277ad5f5a2c78eb
medo_xs <medo_xs@126.com>
2016-10-14 15:24:14 +0800

    第一次提交

---
+++ PictureToExcel/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace PictureToExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 10F);
+            this.button1.Location = new System.Drawing.Point(12, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(500, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始转换";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 294);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 13F);
+            this.label1.Location = new System.Drawing.Point(137, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "点击选择图片，最好不大于1M";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 312);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(500, 35);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 359);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "图片转Excel";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ PictureToExcel/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Drawing;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+
+namespace PictureToExcel
+{
+    public partial class Form1 : Form
+    {
+        private string picPath;
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            progressBar1.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image == null)
+            {
+                MessageBox.Show("先选择图片", "提示");
+                return;
+            }
+
+            pictureBox1.Enabled = false;
+            button1.Enabled = false;
+            progressBar1.Show();
+            Thread t1 = new Thread(new ThreadStart(creatMethod));
+            t1.IsBackground = true;
+            t1.Start();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            pictureBox1_Click(sender, e);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "请选择图片";
+            openFileDialog1.Filter = "图片文件(*.jpg,*.jpeg,*.png,*.gif,*.bmp)|*.jpg|*.jpeg|*.png|*.gif|*.bmp";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                picPath = openFileDialog1.FileName.ToString();
+            }
+
+            try
+            {
+                pictureBox1.Image = Image.FromFile(picPath);
+                label1.Hide();
+            }
+            catch
+            {
+                MessageBox.Show("选择图片加载失败", "警告");
+                label1.Show();
+            }  
+        }
+
+        private void creatMethod()
+        {
+            //create  
+            var file = @"result.xlsx";
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+            }
+            
+            using (var excel = new ExcelPackage(new FileInfo(file)))
+            {
+                var ws = excel.Workbook.Worksheets.Add("Sheet1");
+
+                using (Bitmap bmp = new Bitmap(picPath))
+                {
+                    progressBar1.Minimum = 0;
+                    progressBar1.Maximum = bmp.Height * bmp.Width;
+                    progressBar1.Value = 0;
+
+                    for (var y = 0; y < bmp.Height; y++)
+                    {
+                        ws.Row(y + 1).Height = 5.25;
+
+                        for (var x = 0; x < bmp.Width; x++)
+                        {
+                            Color color = bmp.GetPixel(x, y);
+
+                            ws.Column(x + 1).Width = 1.00;
+                            ws.Cells[y + 1, x + 1].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                            ws.Cells[y + 1, x + 1].Style.Fill.BackgroundColor.SetColor(Color.FromArgb(color.A, color.R, color.G, color.B));
+
+                            progressBar1.Value++;
+                        }
+                    }
+
+                }
+                excel.Save();
+            }
+
+            progressBar1.Hide();
+            pictureBox1.Enabled = true;
+            button1.Enabled = true;
+            MessageBox.Show("转换完成", "提示");
+        }
+    }
+}
